c5ffe3d69d3ff6028bcf293d386a629f85238e5c
Nicholas Dart <nicholasjdart@gmail.com>
2013-11-25 14:03:59 +0000

    Added a HelloWorld function

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,7 @@
+def HelloWorld(saySomething):
+    print "Hello! I was told to say \'" + saySomething + "\'"
+
 print "Hello World, I'm Doing Science?"
 print "Thats nice, I'm programing!"
+
+HelloWorld(" Today's A Good Day!")
